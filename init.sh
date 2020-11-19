71e7d92b0c3e2df900b2421c8b7ab55b7759654d
Kwang Hun <mm9139@naver.com>
2020-11-19 11:21:12 +0900

    cluster init script

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubeadm init --config kubeadm-config.yaml
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
